36474037f90054cd4b79e5ad9c18e713cc3ed2a1
B200001 <imh100.46@bitmesra.ac.in>
2023-05-13 19:14:26 +0530

    demoReact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
+import 'antd/dist/reset.css';
+import { Card } from 'antd';
+import React, { useState, useEffect } from "react";
+import { Divider } from 'antd';
+
+
+const gridStyle = {
+  width: '25%',
+  textAlign: 'center',
+};
 
 function App() {
+  const url = "https://jsonplaceholder.typicode.com/posts/";
+  const [data, setData] = useState([]);
+
+  const fetchInfo = () => {
+    return fetch(url)
+      .then((res) => res.json())
+      .then((d) => setData(d))
+  }
+
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <div className='first'>
+        <div className='count' data-hover="100">User Count</div>
+      </div>
+      <div className="App">
+        {data.map((dataObj, index) => {
+          return (
+            <div className='container'>
+
+
+              <Card className='main_card'
+                // title={dataObj.userId}
+                extra={<a href="#">Github</a>}
+                style={{
+                  width: 300,
+                }}
+              >
+
+                <h2>Title</h2>
+                <div className='title_data'>{dataObj.title}</div>
+                <Divider className='Divider' />
+                <div className='id_main'>#{dataObj.userId}- {dataObj.id}</div>
+                <div className='About' data-hover={dataObj.body}>About</div>
+
+                <Divider className='Divider' />
+                <Button className='button_visit'>Visit Me</Button>
+
+
+              </Card>
+
+
+            </div>
+          )
+        })}
+
+      </div>
     </div>
   );
 }
